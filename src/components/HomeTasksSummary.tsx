966eedfa9e9acb4ee38f611f8d9ab40bd9055601
embarq <embarq.dev@gmail.com>
2021-05-28 11:06:09 +0300

    implement main home section

---
+++ src/components/HomeTasksSummary.tsx
@@ -0,0 +1,40 @@
+import classNames from 'classnames'
+
+const tileShadowBgStyles = `absolute z-0 -bottom-2 -right-0 w-3/4 h-16 filter blur-lg rounded-2xl`;
+
+const HomeTasksSummary: React.FC = () => {
+  return (
+    <section className="grid grid-cols-2 grid-rows-9 gap-3 grid-flow-col p-6">
+      <div className="relative row-span-6 p-3 rounded-2xl z-10 bg-tasks-summary-tile-blue">
+        <div className="relative z-10">
+          <h3 className="font-semibold mb-2">Completed</h3>
+          <p className="text-sm">80 Task</p>
+        </div>
+        <div className={classNames(tileShadowBgStyles, 'hidden bg-blue-200')}></div>
+      </div>
+      <div className="relative row-span-3 p-3 rounded-2xl z-10 bg-tasks-summary-tile-red">
+        <div className="relative z-10">
+          <h3 className="font-semibold mb-2">Canceled</h3>
+          <p className="text-sm">80 Task</p>
+        </div>
+        <div className={classNames(tileShadowBgStyles, 'hidden bg-red-400')}></div>
+      </div>
+      <div className="relative row-span-3 p-3 rounded-2xl z-10 bg-tasks-summary-tile-purple">
+        <div className="relative z-10">
+          <h3 className="font-semibold mb-2">Pending</h3>
+          <p className="text-sm">80 Task</p>
+        </div>
+        <div className={classNames(tileShadowBgStyles, 'hidden bg-purple-400')}></div>
+      </div>
+      <div className="relative row-span-6 p-3 rounded-2xl z-10 bg-tasks-summary-tile-green">
+        <div className="relative z-10">
+          <h3 className="font-semibold mb-2">Ongoing</h3>
+          <p className="text-sm">80 Task</p>
+        </div>
+        <div className={classNames(tileShadowBgStyles, 'hidden bg-green-300')}></div>
+      </div>
+    </section>
+  )
+}
+
+export default HomeTasksSummary
--- src/pages/app/Home.tsx
+++ src/pages/app/Home.tsx
@@ -1,31 +1,67 @@
+import React from 'react'
 import {
+  IonButton,
+  IonButtons,
   IonContent,
   IonHeader,
+  IonIcon,
   IonPage,
   IonTitle,
   IonToolbar,
 } from '@ionic/react'
+import { personOutline } from 'ionicons/icons'
+import HomeTasksSummary from '../../components/HomeTasksSummary'
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle></IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+          <IonToolbar style={{ '--border-width': '0', '--min-height': '64px' }}>
+            <IonTitle size="large" className="px-6">
+              <h2 className="font-semibold text-gray-700">Hi, User</h2>
+              <p className="text-sm font-normal text-gray-500">
+                Let’s make this day productive
+              </p>
+            </IonTitle>
+            <IonButtons slot="primary">
+              <button
+                type="button"
+                className="h-12 w-12 p-2 mr-3 shadow-2xl-primary rounded-2xl text-gray-300">
+                <IonIcon icon={personOutline} slot="icon-only" />
+              </button>
+            </IonButtons>
           </IonToolbar>
         </IonHeader>
-        <p className="text-blue-400 font-bold">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam
-          recusandae odit error beatae quas, dicta natus facilis illum,
-          voluptates reiciendis omnis exercitationem maiores soluta laborum,
-          laudantium possimus quasi perferendis nostrum!
-        </p>
+
+        <IonHeader className="mt-8">
+          <IonToolbar style={{ '--border-width': '0', '--min-height': '64px' }}>
+            <IonTitle size="large" className="px-6">
+              <h2 className="font-semibold text-gray-700">My Tasks</h2>
+            </IonTitle>
+          </IonToolbar>
+        </IonHeader>
+
+        <HomeTasksSummary />
+
+        <IonHeader>
+          <IonToolbar style={{ '--border-width': '0', '--min-height': '64px' }}>
+            <IonTitle size="large" className="px-6">
+              <h2 className="font-semibold text-gray-700">Today Tasks</h2>
+            </IonTitle>
+            <IonButtons slot="primary" className="self-end pr-4">
+              <IonButton fill="clear">
+                <div className="text-sm text-gray-600">View all</div>
+              </IonButton>
+            </IonButtons>
+          </IonToolbar>
+        </IonHeader>
+
       </IonContent>
     </IonPage>
   )

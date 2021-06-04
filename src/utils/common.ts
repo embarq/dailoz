9f734052b41868a3e5866e4c7673b0dce8a78493
embarq <embarq.dev@gmail.com>
2021-06-04 12:03:56 +0300

    add some ts type-guard helpers

---
+++ src/utils/common.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/ban-types
+export const isCallable = (fn: unknown): fn is Function => {
+  return typeof fn === 'function';
+}
+
+/** Valdates `str` arg for being not an empty string.  */
+export const isRealString = (str: unknown): boolean => {
+  return typeof str === 'string' && str.trim().length > 0
+}

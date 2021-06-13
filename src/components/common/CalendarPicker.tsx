3344978280ec7539226c17dac13e2f486b5c016c
embarq <embarq.dev@gmail.com>
2021-06-13 06:13:13 +0300

    implement months and years select

--- src/components/common/CalendarPicker.tsx
+++ src/components/common/CalendarPicker.tsx
@@ -3,12 +3,20 @@ import { IonButton, IonIcon } from '@ionic/react'
 import { chevronBackOutline, chevronForwardOutline } from 'ionicons/icons'
 import classNames from 'classnames'
 
-import eachDayOfInterval from 'date-fns/eachDayOfInterval'
-import isValid from 'date-fns/isValid'
-import format from 'date-fns/format'
-import startOfMonth from 'date-fns/startOfMonth'
-import endOfMonth from 'date-fns/endOfMonth'
-import isSameDay from 'date-fns/isSameDay'
+import {
+  isValid,
+  format,
+  startOfMonth,
+  endOfMonth,
+  isSameDay,
+  addMonths,
+  subMonths,
+  setYear,
+  getYear,
+  eachDayOfInterval,
+} from 'date-fns'
+
+import { getYears } from '../../utils/date'
 
 interface CalendarCell {
   date: Date,
@@ -40,15 +48,31 @@ const CalendarPicker: React.FC<Props> = ({
   const [selectedDate, setSelectedDate] = useState<Date>(new Date())
   const [monthDates, setMonthDates] = useState<ReadonlyArray<CalendarCell>>([])
   const [firstDayOffset, setFirstDayOffset] = useState(0)
+  const [displayYears, setDisplayYears] = useState(false)
 
   const handleSaveClick = () => {
     onSave(selectedDate)
+  }
+
+  const setPrevMonth = () => {
+    setSelectedDate(date => subMonths(date, 1))
+  }
+
+  const setNextMonth = () => {
+    setSelectedDate(date => addMonths(date, 1))
+  }
+
+  const handleSelectYear = (year: number) => {
+    setSelectedDate(date => setYear(date, year))
+    setDisplayYears(false)
   }
 
   const headerParts = {
     month: format(selectedDate, 'LLLL'),
     year: format(selectedDate, 'yyyy'),
   }
+
+  const years: number[] = getYears(selectedDate, 25)
 
   useEffect(() => {
     if (date != null && isValid(date)) {
@@ -74,49 +98,72 @@ const CalendarPicker: React.FC<Props> = ({
   }, [selectedDate])
 
   return (
-    <div className="p-9 pt-12 rounded-2xl bg-white">
-      <header className="flex justify-between items-center w-full px-1 py-2">
-        <h2 className="text-sm text-gray-700 font-bold">
-          {headerParts.month}&nbsp;{headerParts.year}
+    <div className="h-full flex flex-col p-9 pt-12 rounded-2xl bg-white">
+      <header className="flex-none flex justify-between items-center w-full px-1 py-2">
+        <h2 className="">
+          <IonButton
+            onClick={() => setDisplayYears(!displayYears)}
+            fill="clear" size="small" type="button"
+            className="ion-button-unstyled text-sm text-gray-700 font-bold">
+            {headerParts.month}&nbsp;{headerParts.year}
+          </IonButton>
         </h2>
         <div className="centered">
-          {/* TODO: implement switching month backwards */}
-          <IonButton fill="clear" size="small" className="unstyled" type="button">
+          <IonButton onClick={setPrevMonth} fill="clear" size="small" className="ion-button-unstyled" type="button">
             <IonIcon slot="icon-only" icon={chevronBackOutline} size="small" />
           </IonButton>
-          {/* TODO: implement switching month forwards */}
           <div className="w-2"></div>
-          <IonButton fill="clear" size="small" className="unstyled" type="button">
+          <IonButton onClick={setNextMonth} fill="clear" size="small" className="ion-button-unstyled" type="button">
             <IonIcon slot="icon-only" icon={chevronForwardOutline} size="small" />
           </IonButton>
         </div>
       </header>
 
-      <div className="grid grid-cols-7 auto-rows-auto py-8">
-        {weekDays.map(entry => (
-          <div key={entry.id} className="centered w-8 h-8 font-medium text-gray-700 text-sm">
-            {entry.label}
-          </div>
-        ))}
-
-        {monthDates.map(({ date, label, timestamp }, ix) => (
-          <IonButton
-            onClick={() => setSelectedDate(date)}
-            key={timestamp}
-            className={classNames(
-              'centered w-8 h-8 m-0 text-sm font-mediumx rounded-full',
-              ix === 0 && `col-start-${firstDayOffset}`,
-              isSameDay(date, selectedDate) ? 'bg-indigo-500 text-white' : 'bg-transparent text-gray-600',
-              'transition-colors ease-in-out duration-100 hover:bg-indigo-100 hover:text-gray-800',
-            )}
-            fill="clear"
-            type="button">
-            {label}
-          </IonButton>
-        ))}
+      <div className="relative overflow-hidden flex-grow">
+        <div className="h-full grid grid-cols-7 auto-rows-auto py-8">
+          {weekDays.map(entry => (
+            <div key={entry.id} className="centered w-8 h-8 font-medium text-gray-700 text-sm">
+              {entry.label}
+            </div>
+          ))}
+          {monthDates.map(({ date, label, timestamp }, ix) => (
+            <IonButton
+              onClick={() => setSelectedDate(date)}
+              key={timestamp}
+              className={classNames(
+                'centered w-8 h-8 m-0 text-sm font-medium rounded-full',
+                ix === 0 && `col-start-${firstDayOffset}`,
+                isSameDay(date, selectedDate) ? 'bg-indigo-500 text-white' : 'bg-transparent text-gray-600',
+                'transition-colors ease-in-out duration-100 hover:bg-indigo-500',
+              )}
+              fill="clear"
+              type="button">
+              {label}
+            </IonButton>
+          ))}
+        </div>
+        <div className={classNames(
+          'absolute top-0 h-full grid grid-cols-5 auto-rows-auto py-8 bg-white transform',
+          'transition-opacity ease-in-out duration-200',
+          displayYears ? 'translate-y-0 opacity-100' : 'translate-y-full opacity-0'
+        )}>
+          {years.map(year => (
+            <IonButton
+              onClick={() => handleSelectYear(year)}
+              key={year}
+              className={classNames(
+                'centered h-8 m-0 text-sm font-medium rounded-lg',
+                year === getYear(selectedDate) ? 'bg-indigo-500 text-white' : 'bg-transparent text-gray-600'
+              )}
+              fill="clear"
+              type="button">
+              {year}
+            </IonButton>
+          ))}
+        </div>
       </div>
 
-      <footer className="centered pt-1 pb-8">
+      <footer className="flex-none centered">
         <IonButton onClick={onDismiss} fill="outline" size="small" className="w-24 h-9" style={{ '--border-radius': '8px' }}>
           <div className="font-medium">Cancel</div>
         </IonButton>

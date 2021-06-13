4e1636da360bf7063a2e75a6a321dc4228eceb97
embarq <embarq.dev@gmail.com>
2021-06-13 06:09:46 +0300

    implement getYears

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -5,3 +5,12 @@ export const formatDate = (
   const dateTimeFormat = new Intl.DateTimeFormat(void 0, format)
   return dateTimeFormat.format(new Date(value))
 }
+
+export const getYears = (baseDate: Date, count: number): number[] => {
+  const currentYear = baseDate.getFullYear()
+  const half = Math.round(count / 2)
+  return [
+    ...Array.from(Array(half), (_, i) => currentYear - i).reverse(),
+    ...Array.from(Array(half - 1), (_, i) => currentYear + i + 1),
+  ]
+}
